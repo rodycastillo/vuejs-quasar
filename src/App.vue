993af23ce6fbfaba1a47e3a73c6bc2bf9abcacb6
Carlito <rody.castillo.tacuri@gmail.com>
2021-08-12 00:13:40 -0500

    Add proyect

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<q-layout view="lHh Lpr lFf" class="bg-grey-2 text-dark">
+  <SideBar />
+  <q-page-container>
+    <router-view></router-view>
+  </q-page-container>
+</q-layout>
+</template>
+<script>
+import SideBar from './components/SideBar.vue'
+export default {
+  components: { SideBar}
+}
+</script>
